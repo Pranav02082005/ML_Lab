469a22721bde47b5417ac9e8083e6f94452ae11f
Pranav02082005 <pranav.sreeharsha@gmail.com>
2026-06-12 16:58:06 +0000

    Added Elbow Method

--- q7_kmeans.py
+++ q7_kmeans.py
@@ -2,10 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-df = pd.read_csv("datasets/iris1.csv")
+df = pd.read_csv("iris1.csv")
 
 X = df.iloc[:, :2]
+wcss = [KMeans(n_clusters=k, random_state=42).fit(X).inertia_
+        for k in range(1, 11)]
 
+plt.plot(range(1, 11), wcss, marker="o")
+plt.savefig("elbow.png", dpi=100)
+plt.show()
 fig, axes = plt.subplots(1, 3, figsize=(12,4))
 
 for i, k in enumerate([2,3,4]):
@@ -31,4 +36,5 @@ for i, k in enumerate([2,3,4]):
     axes[i].set_title(f"K = {k}")
 
 plt.tight_layout()
+plt.savefig("kmeans.png", dpi=100)
 plt.show()
