c633090c121a3f99160bb1d6495b2a1fcf16a6c4
Pranav02082005 <pranav.sreeharsha@gmail.com>
2026-06-12 12:35:14 +0000

    Updated Q9

--- q9_dbscan.py
+++ q9_dbscan.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 
 from sklearn.cluster import DBSCAN
 # READ CSV
-df = pd.read_csv("iris.csv")
+df = pd.read_csv("iris1.csv")
 
 # REMOVE TARGET COLUMN
-X = df.drop("target", axis=1)
+X = df.drop("Class", axis=1)
 
 # TAKE FIRST 2 FEATURES
 X = X.iloc[:, :2]
 
 model1 = DBSCAN(eps=0.3,min_samples=5)
 labels1 = model1.fit_predict(X)
+
 plt.figure(figsize=(5,5))
 plt.scatter(X.iloc[:,0],X.iloc[:,1],c=labels1)
-
 plt.title("DBSCAN eps=0.3 min_samples=5")
 plt.show()
 
@@ -24,7 +24,6 @@ model2 = DBSCAN(eps=0.5,min_samples=3)
 labels2 = model2.fit_predict(X)
 
 plt.figure(figsize=(5,5))
-
 plt.scatter(X.iloc[:,0],X.iloc[:,1],c=labels2)
 plt.title("DBSCAN eps=0.5 min_samples=3")
 plt.show()
